31832306848256b5722bd3d1464acc5ac76a2def
hitesh kamble <77688759+ghostmirrorlab@users.noreply.github.com>
2022-06-18 14:26:28 +0530

    update git info

--- update.py
+++ update.py
@@ -48,8 +48,8 @@ if UPSTREAM_REPO is not None:
         srun(["rm", "-rf", ".git"])
 
     update = srun([f"git init -q \
-                     && git config --global user.email e.anastayyar@gmail.com \
-                     && git config --global user.name mltb \
+                     && git config --global user.email hiteshkamble920@gmail.com \
+                     && git config --global user.name hitesh920 \
                      && git add . \
                      && git commit -sm update -q \
                      && git remote add origin {UPSTREAM_REPO} \
