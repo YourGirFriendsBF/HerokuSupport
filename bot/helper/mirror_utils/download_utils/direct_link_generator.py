925a3dc365fa525835ff9583d3c0eeea758548a9
Dhruv045 <106961058+Dhruv045@users.noreply.github.com>
2022-07-09 13:16:14 +0530

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -382,7 +382,7 @@ def gdtot(url: str) -> str:
     except:
         raise DirectDownloadLinkException("ERROR: Try in your broswer, mostly file not found or user limit exceeded!")
     return f'https://drive.google.com/open?id={decoded_id}'
-    account = {
+account = {
     'email': APPDRIVE_EMAIL,
     'passwd': APPDRIVE_PASS
     }
