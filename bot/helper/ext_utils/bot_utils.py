6f661e991d19d36808db043b16bf2ba4990d66ae
Dhruv045 <106961058+Dhruv045@users.noreply.github.com>
2022-10-08 12:23:52 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -113,7 +113,7 @@ def get_progress_bar_string(status):
 
 def get_readable_message():
     with download_dict_lock:
-        msg = f"Powered By <b>Dhruv Mirror Updates</b>"
+        msg = f"<b>Only for You Bish!</b>"
         if STATUS_LIMIT is not None:
             tasks = len(download_dict)
             global pages
